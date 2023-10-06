65862cffe14a2bd8ff18bea37b648194bcb3e555
Elliot <thehumanej@gmail.com>
2023-10-06 18:28:12 -0500

    added motor controllers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +21,10 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private final CANSparkMax frontright_motor = new CANSparkMax(1,MotorType.kBrushless);
+  private final CANSparkMax backright_motor = new CANSparkMax(2,MotorType.kBrushless);
+  private final CANSparkMax frontleft_motor = new CANSparkMax(3,MotorType.kBrushless);
+  private final CANSparkMax backleft_motor = new CANSparkMax(4,MotorType.kBrushless);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -78,7 +84,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    // m_robotDrive.
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
